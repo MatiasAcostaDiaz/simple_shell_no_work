70b183cb0af6e8af1512420f4a79c1216d8ac788
Matias Acosta <2277@holbertonschool.com>
2020-11-13 07:58:28 -0300

    New commit

---
+++ shell.h
@@ -0,0 +1,11 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#define TRUE 1
+#define BUFFSIZE 1024
+#endif
